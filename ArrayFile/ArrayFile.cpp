8ae40767032e23b55da269e7831245d28aa96a51
ChervinskaAnna <chervinska.anna@chnu.edu.ua>
2023-02-28 21:19:21 +0200

    3.2

--- ArrayFile/ArrayFile.cpp
+++ ArrayFile/ArrayFile.cpp
@@ -1,251 +1,406 @@
-﻿
-/* Варіант 11 
-Завдання 1: Задано одновимірний масив А розміру N. Першу половину елементів масиву 
-помножити на 2, а другу - на 3.
-*/
+﻿// ArrayFile.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
 #include <iostream>
 #include <fstream>
-#include <stdio.h>
+#include <ios>
+#include <vector>
 #include <conio.h>
+#include <time.h>
+
 using namespace std;
 
-typedef struct {
-	int length;
-	int data[200];
-} TArray;
+typedef double* pDouble;
 
 
-void saveArrayToFile(TArray arr) {
-	char filename[200];
-	char answer;
-	cout << "Bazhaete zberigti?" << endl;
-	cin >> answer;
-	if (answer == 'N' or answer == 'n')
-		return;
+const int MAX_SIZE = 560;
 
-	cout << "FileName?" << endl;
-	cin >> filename;
+/*
+*   ConsoleInputArrayDouble
+*
+*/
+int ConsoleInputSizeArray(const int sizeMax)
+{
+    int size = 0;
+    do {
+        cout << " Input size Array ( 0< 1 < " << sizeMax << " ) ";
+        cin >> size;
+    } while (size <= 0 || size >= sizeMax);
+    return size;
+}
+/*
+*   ConsoleInputArrayDouble
+*
+*/
+int ConsoleInputArray(int sizeMax, double A[])
+{
+    int size = ConsoleInputSizeArray(sizeMax);
+    for (int i = 0; i < size; i++) {
+        cout << " Array[ " << i << "] "; cin >> A[i];
+    }
+    return size;
+}
 
-	cout << filename << endl;
+/*
+*   RndInputArrayDouble
+*
+*/
+int RndInputArray(int sizeMax, double A[])
+{
+    int size = ConsoleInputSizeArray(sizeMax);
+    int r1 = 0, r2 = 0;
+    srand(size);
 
+    for (int i = 0; i < size; i++) {
+        r1 = rand();
+        r2 = rand();
+        A[i] = 100.0 * r1;
+        A[i] = A[i] / (1.0 + r2);
+        cout << A[i] << "   ";
+    }
+    return size;
+}
 
-	ofstream outfile;
-	remove(filename);
-	outfile.open(filename);
+int ConsoleInputDynamicArrayNew(int sizeMax, pDouble& pA)
+{
+    int size = ConsoleInputSizeArray(sizeMax);
+    pA = new double[size];
+    if (pA == nullptr) { return 0; }
+    for (int i = 0; i < size; i++) {
+        cout << " Array[ " << i << "] "; cin >> pA[i];
+    }
+    return size;
+}
 
-	for (int i = 0; i < arr.length; i++) {
-		outfile << " " << arr.data[i];
-	}
+int ConsoleInputDynamicArray_calloc(int sizeMax, pDouble& pA)
+{
+    int size = ConsoleInputSizeArray(sizeMax);
+    pA = (double*)calloc(size, sizeof(double));      // pA = (double*)malloc(size * sizeof(double)); 
+    if (pA == nullptr) { return 0; }
+    for (int i = 0; i < size; i++) {
+        cout << " Array[ " << i << "] "; cin >> pA[i];
+    }
+    return size;
+}
 
-	outfile.close();
+void ConsoleInputVector(int sizeMax, vector<double>& A)
+{
+    int size = ConsoleInputSizeArray(sizeMax);
+    double d;
+    for (int i = 0; i < size; i++) {
+        cout << " Array[ " << i << "] "; cin >> d; A.push_back(d);
+    }
+    return;
 }
 
 
-TArray readFromFile() {
-	char filename[200];
-	TArray result;
-	result.length = 0;
+/*
+*  WriteArrayTextFile
+*
+*/
 
-	cout << "Enter filename" << endl;
-	cin >> filename;
+void WriteArrayTextFile(int n, double* arr, const char* fileName)
+{
+    ofstream fout(fileName);
+    if (fout.fail()) return;
+    fout << n << endl;
+    for (int i = 0; i < n; i++)
+        fout << arr[i] << "   ";
+    fout.close(); //
+}
+void WriteArrayConsole(int n, double* arr)
+{
+    cout << "Array:\n";
+    for (int i = 0; i < n; i++) {
+        cout << "[" << i + 1 << "]" << ":" << arr[i] << "\n";
+    }
+}
+/*
+*  ReadArrayTextFile
+*
+*/
 
-	ifstream inFile;
-	inFile.open(filename);
 
-	if (inFile.is_open())
-	{
-		for (int i = 0; i < 200; i++)
-		{
-			inFile >> result.data[i];
-			result.length++;
-			cout << result.data[i] << " ";
-
-			if (inFile.eof())
-				break;
-		}
-
-		inFile.close(); // CLose input file
-	}
-
-	return result;
+int ReadArrayTextFile(int n, double* arr, const char* fileName)
+{ 
+    int size;
+    ifstream fin(fileName);
+    if (fin.fail()) return 0;
+    fin >> size;
+    if (size <= 0) return 0;
+    if (size > n) size = n;
+    for (int i = 0; i < n; i++)
+        fin >> arr[i];
+    fin.close();
+    return size;
 }
 
 
+void WriteArrayBinFile(int n, double* arr, const char* fileName)
+{
+    //ios_base
+    ofstream bfout(fileName, ios_base::binary);
+    if (bfout.fail()) return;
+    bfout.write((const char*)&n, sizeof(int));
+    std::streamsize  cn = static_cast<std::streamsize>(n) * sizeof(double);
+    bfout.write((const char*)arr, cn);
+    bfout.close();
+}
 
-int main() {
-    int i;
-	TArray arr;
-	arr = readFromFile();
-	if (arr.length == 0)
-	{
-		cout << "File is not found!!!11" << endl;
+int ReadArrayBinFile(int n, double* arr, const char* fileName)
+{
+    int size = 0;
+    ifstream bfin(fileName, ios_base::binary);
+    if (bfin.fail()) return 0;
+    bfin.read((char*)&size, sizeof(int));
+    if (size <= 0) return 0;
+    if (size > n) size = n;
+    bfin.read((char*)arr, static_cast<std::streamsize>(size) * sizeof(double));
+    bfin.close();
+    // ssdhs
+    return size;
+}
 
-		cout << "Vedite Length" << endl;
-		cin >> arr.length;
+void ShowMainMenu()
+{
+    system("cls");
+    cout << "    Main Menu  \n";
+    cout << "    1.  Task 1  \n";
+    cout << "    2.  Task 2  \n";
+    cout << "    3.  Task 3  \n";
+}
 
-		for (i = 0; i < arr.length; i++) {
+void MenuTask()
+{
+    cout << "     Menu Task   \n";
+    cout << "    1.  Local array  \n";
+    cout << "    2.  Dynamic array 1 \n";
+    cout << "    3.  Dynamic array 2  new \n";
+    cout << "    4.  Dynamic array : vector \n";
+    cout << "    5.  Exit \n";
+}
 
-			cout << "[" << i + 1 << "]" << ":";
-			cin >> arr.data[i];
-		}
+/*
+* Задано одновимірний масив А розміру 2N.
+* Побудувати два масиви В і С розміру N,
+* включивши  у масив В елементи масиву А з парними індексами,
+* а у С - з непарними.
+*****************
+*  A - in
+*  B, C - out
+*/
+void  TestVariant(int N, double* A, double* B, double* C) {
+    for (int i = 0; i < N; i++) {
+        B[i] = A[2 * i];
+        C[i] = A[2 * i + 1];
+    }
+}
+/*
+*  Task  Var
+*
+*
+*/
+void TaskV()
+{
+    char ch = '5';
+    do {
+        system("cls");
+        MenuTask();
+        ch = getchar();
+        getchar();
+        switch (ch) {
+        case '1': cout << " 1 "; break;
+        case '2': cout << " 2 "; break;
+            //
+        case '5': return;
+        }
+        cout << " Press any key and enter\n";
+        ch = getchar();
+    } while (ch != 27);
 
-		saveArrayToFile(arr);
-	}
+}
+
+void ArrayLocal()
+{
+    double A[1000], B[500], C[500];
+    int n;
+    char ch = '5';
+    do {
+        system("cls");
+        MenuTask();
+        ch = getchar();
+        getchar();
+        switch (ch) {
+        case '1': cout << " 1 "; break;
+        case '2': cout << " 2 "; break;
+            //
+        case '5': return;
+        }
+        cout << " Press any key and enter\n";
+        ch = getchar();
+    } while (ch != 27);
+
+}
 
 
-    for (i = 0; i < arr.length; i++) {
-        if (i < arr.length / 2) {
-			arr.data[i] = arr.data[i] * 2;
+int InputArray(int sizeMax, double arr[]) {
+    char ch;
+    int i, arrLen;
+
+    cout << "\n  Select Input method:   \n";
+    cout << "    1.  Console all \n";
+    cout << "    2.  Console - size, array - random \n";
+    cout << "    3.  File1.txt \n";
+    do {
+        ch = _getch();
+    } while (ch != '1' && ch != '2' && ch != '3');
+
+    switch (ch) {
+    case '1':
+        cout << "    1.  Console all \n";
+        arrLen = ConsoleInputArray(MAX_SIZE, arr);
+        break;
+    case '2':
+        cout << "    2.  Console - size, array - random \n";
+        arrLen = RndInputArray(MAX_SIZE, arr);
+        break;
+    case '3':
+        cout << "    3.  File 1.txt \n";
+        arrLen = ReadArrayTextFile(MAX_SIZE, arr, "File1.txt");
+        break;
+    }
+
+    return arrLen;
+}
+
+void OutputArray(int arrLen, double arr[]) {
+    char ch;
+
+    cout << "\n  Select Output method:   \n";
+    cout << "    1.  Console \n";
+    cout << "    2.  File2.txt \n";
+    cout << "    3.  Console & File2.txt \n";
+
+    do {
+        ch = _getch();
+    } while (ch != '1' && ch != '2' && ch != '3');
+
+    switch (ch) {
+    case '1':
+        WriteArrayConsole(arrLen, arr);
+        break;
+    case '2':
+        WriteArrayTextFile(arrLen, arr, "File2.txt");
+        break;
+    case '3':
+        WriteArrayConsole(arrLen, arr);
+        WriteArrayTextFile(arrLen, arr, "File2.txt");
+        break;
+    }
+}
+
+void Task1() {
+    char ch;
+    int i, arrLen;
+    double arr[MAX_SIZE];
+
+    arrLen = InputArray(MAX_SIZE, arr);
+
+    for (i = 0; i < arrLen; i++) {
+        if (i < arrLen / 2) {
+            arr[i] = arr[i] * 2;
         }
         else {
-			arr.data[i] = arr.data[i] * 3;
+            arr[i] = arr[i] * 3;
         }
     }
 
-	cout << "Res\n";
-	for (i = 0; i < arr.length; i++) {
-		cout << arr.data[i] << " ";
-	}
+    OutputArray(arrLen, arr);
 
-	saveArrayToFile(arr);
+}
+void Task2() {
+    cout << " \n   Task 2 is not ready \n";
+    int i, N;
+    int indV;
+    int min = 10000010;
 
-    cout << endl;
+    cout << "Vedite N" << endl;
+    cin >> N;
 
-    _getch();
-    return 0;
+    int massive_A[100];
+
+    for (i = 0; i < N; i++) {
+
+        cout << "[" << i + 1 << "]" << ":";
+        cin >> massive_A[i];
+    }
+
+    for (i = 0; i < N; i++) {
+        if (massive_A[i] < 0) {
+            indV = i;
+            break;
+        }
+    }
+
+    for (i = indV; i < N; i++) {
+        if ((massive_A[i] < min) && (massive_A[i] > 0) &&
+            (massive_A[i] % 2 == 0)) {
+            min = massive_A[i];
+        }
+    }
+
+    for (i = N; i > 0; i--) {
+        if (min == massive_A[i]) {
+            cout << i;
+            break;
+        }
+    }
 }
 
+void Task3() {
+    cout << " \n   Task 3 is not ready \n";
+}
 
+void SelectTask() {
+    char ch;
 
+    do {
+        cout << "\n  Select Task:   \n";
+        cout << "    1.  Task 1 \n";
+        cout << "    2.  Task 2 \n";
+        cout << "    3.  Task 3 \n";
+        cout << "    4.  Exit \n";
 
- /*Завдання 2: Знайти номер останнього мінімального елемента серед парних додатних
-елементів, що лежать правіше першого від’ємного елемента
+        ch = _getch();
+        switch (ch) {
+        case '1': Task1(); break;
+        case '2': Task2(); break;
+        case '3': Task3(); break;
+        }
+    } while (ch != '4');
+}
 
-#include <iostream>
-#include <stdio.h>
-#include <conio.h>
-using namespace std;
-
-int main() {
-	int i, N;
-	int indV;
-	int min = 10000010;
-
-	cout << "Vedite N" << endl;
-	cin >> N;
-
-	int massive_A[100];
-
-	for (i = 0; i < N; i++) {
-
-		cout << "[" << i + 1 << "]" << ":";
-		cin >> massive_A[i];
-	}
-
-	for (i = 0; i < N; i++) {
-		if (massive_A[i] < 0) {
-			indV = i;
-			break;
-		}
-	}
-
-	for (i = indV; i < N; i++) {
-		if ((massive_A[i] < min) && (massive_A[i] > 0) &&
-			(massive_A[i] % 2 == 0)) {
-			min = massive_A[i];
-		}
-	}
-
-	for (i = N; i > 0; i--) {
-	 if (min == massive_A[i]){
-	  cout << i;
-	  break;
-	 }
-	}
-
-	getch();
-	return 0;
-}*/
-
-
-/*Завдання 3: На площині задано множину точок з координатами 
- , , , ,..., , ,
-n n
-x y x y x y 1 1 2 2 n  200
-і координати вершин трикутника 
-  a a A x , y ,   b b B x , y ,   c c C x , y
-. Розробити програму, яка 
-обчислює кількість точок заданої множини, що належать 
-ABC */
-
-/*#include "pch.h"
-#include <iostream>
-#include <stdio.h>
-#include <conio.h>
-
-using namespace winrt;
-using namespace Windows::Foundation;
-using namespace std;
-typedef struct {
-	double x, y;
-} TPoint;
 
 int main()
 {
-	int N;
-	TPoint A, B, C;
-	TPoint Points[200];
-	double PL1, PL2, PL3, PL;
-	double Nx, Ny;
-	int i, j;
-	int inside=0;
 
-	cout << "Enter number points" << endl;
-	cin >> N;
+    SelectTask();
 
 
-	cout << "Enter Ax" << endl;
-	cin >> A.x;
 
-	cout << "Enter Ay" << endl;
-	cin >> A.y;
+    //getchar();
 
-	cout << "Enter Bx" << endl;
-	cin >> B.x;
+    return 1;
 
-	cout << "Enter By" << endl;
-	cin >> B.y;
-
-	cout << "Enter Cx" << endl;
-	cin >> C.x;
-
-	cout << "Enter Cy" << endl;
-	cin >> C.y;
-
-	for (i = 0; i < N; i++) {
-		printf("Enter Point[%d].X: ", i);
-		cin >> Points[i].x;
-
-		printf("Enter Point[%d].Y: ", i);
-		cin >> Points[i].y;
-	}
-
-	PL = abs(((A.x * B.y + B.x * C.y + C.x * A.y) - (A.y * B.x + B.y * C.x + C.y * A.x)) / 2);
-
-	for (i = 0; i < N; i++) {
-		PL1 = abs(((Points[i].x * B.y + B.x * C.y + C.x * Points[i].y) - (Points[i].y * B.x + B.y * C.x + C.y * Points[i].x)) / 2);
-		PL2 = abs(((A.x * Points[i].y + Points[i].x * C.y + C.x * A.y) - (A.y * Points[i].x + Points[i].y * C.x + C.y * A.x)) / 2);
-		PL3 = abs(((A.x * B.y + B.x * Points[i].y + Points[i].x * A.y) - (A.y * B.x + B.y * Points[i].x + Points[i].y * A.x)) / 2);
-
-
-		if (PL == PL1 + PL2 + PL3) {
-			inside++;
-		}
-	}
-	cout << "RES" << endl << inside;
-
-	getch();
-	return 0;
 }
-*/
 
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
